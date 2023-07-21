4e44a6c9688c9e8ad6da1709d65de4da4f9ffa02
Jibi George <jibiyyan@gmail.com>
2023-07-21 19:22:14 +0530

    Frontend completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <h1 className='w-full h-screen flex justify-center items-center text-2xl'>Google Cloud Translate</h1>
+    <div className='w-full h-screen flex flex-col'>
+      <Header />
+      <div className="w-full h-full flex flex-col items-center pt-[80px]">
+        <div className='w-[250px] h-[300xp] bg-white p-3'>
+          <div className='w-full h-[200px] overflow-hidden'>
+            <img src='https://fostervictor.com/wp-content/uploads/2019/12/AtomicHabits_1book-768x993.png' style={{ objectFit: 'contain', width: '100%', height: '100%' }} />
+          </div>
+          <div className='w-full overflow-hidden font-medium font-serif'>
+            <p className='text-black'>Atomic Habits is the most comprehensive and practical guide on how to create good habits, break bad ones, and get 1 percent better every day. I do not believe you will find a more actionable book on the subject of habits and improvement. If you're having trouble changing your habits, the problem isn't you.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
