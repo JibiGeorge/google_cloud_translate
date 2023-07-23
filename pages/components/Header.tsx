9f319721f5f71c21d9c558955c9f931e7e5c4ca4
Jibi George <jibiyyan@gmail.com>
2023-07-23 23:58:21 +0530

    Added language switcher and store in cookies for previous set language shows when revisting the page

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { ChangeEvent } from 'react'
 
 const Header = () => {
+    const { push, locale, pathname } = useRouter();
+
+    const handleLanguage = (e: ChangeEvent<HTMLSelectElement>) => {
+        document.cookie = `NEXT_LOCALE=${e.target.value}`
+        push(pathname, pathname, { locale: e.target.value })
+    }
     return (
         <div className='w-full h-auto fixed bg-white px-10 py-2 flex fle-row justify-between items-center'>
             <h1 className='text-black text-2xl font-sans font-bold'>Google Language Translator</h1>
             <div className=''>
-                <select name="" id="" className='text-black border border-black w-[120px] rounded-[3px]'>
+                <select value={locale} name="" id="" className='text-black border border-black w-[120px] rounded-[3px]' onChange={handleLanguage}>
                     <option value="en">English</option>
                     <option value="hi">Hindi</option>
                     <option value="it">Italin</option>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import { GetServerSidePropsContext } from "next";
 import Header from "./components/Header";
 import { Translate } from "@google-cloud/translate/build/src/v2";
+import bookData from '../public/data.json'
 
 const credentials = {
   type: process.env.GOOGLE_TRANSLATE_ACCOUNT_TYPE,
@@ -15,54 +17,58 @@ const credentials = {
   universe_domain: process.env.GOOGLE_TRANSLATE_UNIVERSE_DOMAIN
 }
 
-export default function Home({ data }) {
-  console.log('data', data);
-
+export default function Home({ data }: any) {
   return (
     <div className='w-full h-screen flex flex-col'>
       <Header />
       <div className="w-full h-full flex flex-col items-center pt-[80px]">
-        <div className='w-[250px] h-[300xp] bg-white p-3'>
-          <div className='w-full h-[200px] overflow-hidden'>
-            <img src={data.image} style={{ objectFit: 'contain', width: '100%', height: '100%' }} />
-          </div>
-          <div className='w-full overflow-hidden font-medium font-serif'>
-            <p className='text-black'>{data.about}</p>
-          </div>
+        <div className="w-full h-auto flex gap-3 flex-row justify-center flex-wrap">
+          {data.map((item: any, index: number) => (
+            <div className='w-[250px] max-h-[500px] bg-white p-3' key={index}>
+              <h1 className="text-center text-2xl font-bold underline text-[#a03131] mb-3 truncate">{item.title}</h1>
+              <div className='w-full h-[200px] overflow-hidden mb-4'>
+                <img src={item.image} style={{ objectFit: 'contain', width: '100%', height: '100%' }} />
+              </div>
+              <div className='w-full overflow-hidden font-medium font-serif'>
+                <p className='text-black overflow-hidden' style={{display: '-webkit-box', WebkitLineClamp: 6, WebkitBoxOrient: 'vertical'}}>{item.about}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   )
 }
 
-export async function getServerSideProps() {
-  const data = {
-    image: 'https://fostervictor.com/wp-content/uploads/2019/12/AtomicHabits_1book-768x993.png',
-    about: `Atomic Habits is the most comprehensive and practical guide on how to create good habits, break bad ones, and get 1 percent better every day. I do not believe you will find a more actionable book on the subject of habits and improvement. If you're having trouble changing your habits, the problem isn't you.`
-  }
+export async function getServerSideProps({ locale }: GetServerSidePropsContext) {
+
+  const data = bookData;
 
   const translate = new Translate({
     credentials: credentials,
     projectId: credentials.project_id
   })
 
-  const translateText = async (text, language) => {
+  const translateText = async (text: string) => {
     try {
-      let [response] = await translate.translate(text, language)
+      let [response] = await translate.translate([text], locale || 'en')
       return response;
     } catch (error) {
       console.log('error found in detect', error);
       return 0;
     }
   }
-  const targetLanguage = 'hi';
 
-  const translatedAbout = await translateText(data.about, targetLanguage);
+  const translatedData = await Promise.all(data.map(async (item) => {
+    const translatedTitle = await translateText(item.title);
+    const translatedAbout = await translateText(item.about);
 
-  const translatedData = {
-    ...data,
-    about: translatedAbout,
-  };
+    return {
+      ...item,
+      title: translatedTitle,
+      about: translatedAbout,
+    };
+  }));
 
   return {
     props: {
